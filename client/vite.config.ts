663c7d08bca84ca77489070dafd9eeeb1bdfdaf2
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2025-07-04 16:32:39 -0700

    WHY WHY WHY WHY

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig(({ mode }) => {
   const isDev = mode === "development";
   return {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), vercel()],
     server: {
       host: isDev ? true : false,
     },
